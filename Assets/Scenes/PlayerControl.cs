676964b9493d86a26b7e29480ab60fe9bf10b277
Tanndlin <tanndlin@gmail.com>
2023-09-09 19:20:14 -0400

    Remove leftover log

--- Assets/Scenes/PlayerControl.cs
+++ Assets/Scenes/PlayerControl.cs
@@ -20,7 +20,6 @@ public class PlayerControl : MonoBehaviour {
             Vector3 movement = new Vector3 (move.x, 0f, move.y);
             transform.rotation = Quaternion.Slerp (transform.rotation, Quaternion.LookRotation (movement), 0.15f);
             transform.Translate (movement * speed * Time.deltaTime, Space.World);
-            Debug.Log (move);
         }
     }
 }
