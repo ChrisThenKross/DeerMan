19ea068a1ba3036ef485c64a6ab2bdd85efc664a
Cristian <christhenkross@gmail.com>
2023-09-13 22:54:50 -0400

    Fixed PlayerControl
    Moved camera onto player

--- Assets/Scenes/PlayerControl.cs
+++ Assets/Scenes/PlayerControl.cs
@@ -3,23 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class PlayerControl : MonoBehaviour {
+public class PlayerControl : MonoBehaviour
+{
     public float speed;
     private Vector2 move;
 
-    public void OnMove (InputValue value) {
-        move = value.Get<Vector2> ();
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        move = context.ReadValue<Vector2>();
     }
 
-    void Start () {
+    void Start()
+    {
 
     }
 
-    void Update () {
-        if (move.sqrMagnitude > 0.1f) {
-            Vector3 movement = new Vector3 (move.x, 0f, move.y);
-            transform.rotation = Quaternion.Slerp (transform.rotation, Quaternion.LookRotation (movement), 0.15f);
-            transform.Translate (movement * speed * Time.deltaTime, Space.World);
+    void Update()
+    {
+        if (move.sqrMagnitude > 0.1f)
+        {
+            Vector3 movement = new Vector3(move.x, 0f, move.y);
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), 0.15f);
+            transform.Translate(movement * speed * Time.deltaTime, Space.World);
         }
     }
 }
