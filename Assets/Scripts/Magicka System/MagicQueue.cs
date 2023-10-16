5c7bfba87a7f2d16c26372ac9be9a08b1d290834
Cristian <christhenkross@gmail.com>
2023-10-16 18:23:35 -0400

    medium and big fireball added

--- Assets/Scripts/Magicka System/MagicQueue.cs
+++ Assets/Scripts/Magicka System/MagicQueue.cs
@@ -6,9 +6,15 @@ using TMPro;
 
 public class MagicQueue : MonoBehaviour
 {
-    private Queue<string> spells = new Queue<string> ();
+    //private Queue<string> spells = new Queue<string> ();
+    private List<string> spells = new List<string> ();
     private InputManager playerControls;
     [SerializeField] private TMP_Text spellQueue;
+    [SerializeField] private Transform castPoint;
+    // LIST OF ALL OUR POSSIBLE SPELLS
+    [SerializeField] private Spell med_fireball;
+    [SerializeField] private Spell big_fireball;
+    [SerializeField] private Spell spellToCast;
 
     private void Awake()
     {
@@ -45,20 +51,32 @@ public class MagicQueue : MonoBehaviour
 
     public void AddSpell (string command)
     {
-        spells.Enqueue (command);
+        spells.Add (command);
         UpdateQueuedSpells();
     }
 
-    public void RunNextSpell(InputAction.CallbackContext context)
+    public void castSpell(InputAction.CallbackContext context)
     {
         if (context.started)
         {
             if (spells.Count > 0)
             {
+                spells.Sort();
                 string spell = string.Join(",", spells);
-                spells.Clear ();
+                spells.Clear();
                 spellQueue.text = "Casted spell with " + spell;
                 Debug.Log(spell);
+
+                // instantiate whatever spell was just created
+
+                // please replace after this works
+                if (spell.Equals("fireball,fireball"))
+                {
+                    Instantiate(med_fireball, castPoint.position, castPoint.rotation);
+                } else if (spell.Equals("fireball,fireball,fireball"))
+                {
+                    Instantiate(big_fireball, castPoint.position, castPoint.rotation);
+                }
             }
             else
             {
