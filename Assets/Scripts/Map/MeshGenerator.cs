cea8666d4ffa6dd72406d7e6d3aaba492da54b03
tanndlin <tanndlin@gmail.com>
2023-11-18 19:15:50 -0500

    Generate Seperate Floor Mesh

--- Assets/Scripts/Map/MeshGenerator.cs
+++ Assets/Scripts/Map/MeshGenerator.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 using Unity.AI.Navigation;
 using UnityEngine;
 
@@ -29,7 +31,6 @@ public class MeshGenerator : MonoBehaviour {
         }
     }
 
-    //TODO: Add floor mesh
     public void GenerateMesh (TileType[, ] map, float squareSize) {
         // Move this object up by the wall height
         // Its easier to make a floor than a ceiling
@@ -41,39 +42,17 @@ public class MeshGenerator : MonoBehaviour {
 
         ResetTrees ();
 
-        squareGrid = new SquareGrid (map, squareSize);
-        vertices = new List<Vector3> ();
-        triangles = new List<int> ();
-
-        for (int x = 0; x < squareGrid.squares.GetLength (0); x++)
-            for (int y = 0; y < squareGrid.squares.GetLength (1); y++)
-                TriangulateSquare (squareGrid.squares[x, y]);
-
-        DistributeTrees ();
-
-        Mesh mesh = new Mesh ();
-        GetComponent<MeshFilter> ().mesh = mesh;
-
-        // TODO: Make floor mesh
-
-        mesh.vertices = vertices.ToArray ();
-        mesh.triangles = triangles.ToArray ();
-        mesh.RecalculateNormals ();
-
-        GenerateMeshCollider (mesh);
+        GenerateWalls();
+        ReserVertexIndecies();
+        GenerateFloor();
 
         watch.Stop ();
         Debug.Log ($"Generated mesh with {vertices.Count} vertices and {triangles.Count / 3} triangles in {watch.ElapsedMilliseconds}ms");
 
-        // Expand floor to cover entire map by dimensions
-        Vector3 floorScale = new Vector3 (map.GetLength (0) * squareSize / 10, 1, map.GetLength (1) * squareSize / 10);
-        floor.transform.localScale = floorScale;
-        floor.transform.position = new Vector3 (0, 0, 0);
-
         // Put player somewhere not in a wall
         Vector2 playerPos = new Vector2 (0, 0);
         while (!IsValidPlayerPosition (playerPos)) {
-            playerPos = new Vector2 (Random.Range (0, map.GetLength (0)), Random.Range (0, map.GetLength (1)));
+            playerPos = new Vector2 (UnityEngine.Random.Range (0, map.GetLength (0)), UnityEngine.Random.Range (0, map.GetLength (1)));
         }
 
         Vector3 playerpos3 = new Vector3 (playerPos.x, .5f, playerPos.y);
@@ -86,6 +65,61 @@ public class MeshGenerator : MonoBehaviour {
         // Bake NavMesh
         NavMeshSurface navMesh = floor.GetComponent<NavMeshSurface> ();
         navMesh.BuildNavMesh ();
+    }
+
+    public void GenerateWalls(){
+        squareGrid = new SquareGrid (map, squareSize);
+        vertices = new List<Vector3> ();
+        triangles = new List<int> ();
+
+        for (int x = 0; x < squareGrid.squares.GetLength (0); x++)
+            for (int y = 0; y < squareGrid.squares.GetLength (1); y++)
+                TriangulateSquare (squareGrid.squares[x, y], false);
+
+        DistributeTrees ();
+
+        Mesh mesh = new()
+        {
+            vertices = vertices.ToArray(),
+            triangles = triangles.ToArray()
+        };
+        mesh.RecalculateNormals ();
+
+        GetComponent<MeshFilter> ().mesh = mesh;
+        GenerateMeshCollider (mesh);
+    }
+
+    public void GenerateFloor(){
+        vertices = new List<Vector3> ();
+        triangles = new List<int> ();
+
+        for (int x = 0; x < squareGrid.squares.GetLength (0); x++)
+            for (int y = 0; y < squareGrid.squares.GetLength (1); y++)
+                TriangulateSquare (squareGrid.squares[x, y], true);
+
+        Mesh mesh = new()
+        {
+            vertices = vertices.ToArray(),
+            triangles = triangles.ToArray()
+        };
+
+        mesh.RecalculateNormals ();
+        floor.GetComponent<MeshFilter> ().mesh = mesh;
+        floor.GetComponent<MeshCollider> ().sharedMesh = mesh;
+    }
+
+    private void ReserVertexIndecies () {
+        foreach (Square square in squareGrid.squares) {
+            square.topLeft.vertexIndex = -1;
+            square.topRight.vertexIndex = -1;
+            square.bottomRight.vertexIndex = -1;
+            square.bottomLeft.vertexIndex = -1;
+
+            square.centerTop.vertexIndex = -1;
+            square.centerRight.vertexIndex = -1;
+            square.centerBottom.vertexIndex = -1;
+            square.centerLeft.vertexIndex = -1;
+        }
     }
 
     void GenerateMeshCollider (Mesh mesh) {
@@ -120,73 +154,87 @@ public class MeshGenerator : MonoBehaviour {
         trees.Clear ();
     }
 
-    void TriangulateSquare (Square square) {
-        switch (square.configuration) {
+    void TriangulateSquare (Square square, bool invert) {
+        // Use this lambda so that we can optionally generate walls
+        // without the use of an if statement for each call
+        Action<Node, Node> TryCreateWall = (Node a, Node b) => {
+            if (!invert) {
+                CreateWall (a, b);
+            }
+        };
+
+        int configuration = square.configuration;
+        if (invert) {
+            // Negate state of each node and mask last 4 bits
+            configuration = ~configuration & 15;
+        }
+
+        switch (configuration) {
             case 0:
                 break;
 
                 // 1 points:
             case 1:
                 MeshFromPoints (square.centerBottom, square.bottomLeft, square.centerLeft);
-                CreateWall (square.centerLeft, square.centerBottom);
+                TryCreateWall (square.centerLeft, square.centerBottom);
                 break;
             case 2:
                 MeshFromPoints (square.centerRight, square.bottomRight, square.centerBottom);
-                CreateWall (square.centerBottom, square.centerRight);
+                TryCreateWall (square.centerBottom, square.centerRight);
                 break;
             case 4:
                 MeshFromPoints (square.centerTop, square.topRight, square.centerRight);
-                CreateWall (square.centerRight, square.centerTop);
+                TryCreateWall (square.centerRight, square.centerTop);
                 break;
             case 8:
                 MeshFromPoints (square.topLeft, square.centerTop, square.centerLeft);
-                CreateWall (square.centerTop, square.centerLeft);
+                TryCreateWall (square.centerTop, square.centerLeft);
                 break;
 
                 // 2 points:
             case 3:
                 MeshFromPoints (square.centerRight, square.bottomRight, square.bottomLeft, square.centerLeft);
-                CreateWall (square.centerLeft, square.centerRight);
+                TryCreateWall (square.centerLeft, square.centerRight);
                 break;
             case 6:
                 MeshFromPoints (square.centerTop, square.topRight, square.bottomRight, square.centerBottom);
-                CreateWall (square.centerBottom, square.centerTop);
+                TryCreateWall (square.centerBottom, square.centerTop);
                 break;
             case 9:
                 MeshFromPoints (square.topLeft, square.centerTop, square.centerBottom, square.bottomLeft);
-                CreateWall (square.centerTop, square.centerBottom);
+                TryCreateWall (square.centerTop, square.centerBottom);
                 break;
             case 12:
                 MeshFromPoints (square.topLeft, square.topRight, square.centerRight, square.centerLeft);
-                CreateWall (square.centerRight, square.centerLeft);
+                TryCreateWall (square.centerRight, square.centerLeft);
                 break;
             case 5:
                 MeshFromPoints (square.centerTop, square.topRight, square.centerRight, square.centerBottom, square.bottomLeft, square.centerLeft);
-                CreateWall (square.centerLeft, square.centerTop);
-                CreateWall (square.centerRight, square.centerBottom);
+                TryCreateWall (square.centerLeft, square.centerTop);
+                TryCreateWall (square.centerRight, square.centerBottom);
                 break;
             case 10:
                 MeshFromPoints (square.topLeft, square.centerTop, square.centerRight, square.bottomRight, square.centerBottom, square.centerLeft);
-                CreateWall (square.centerBottom, square.centerLeft);
-                CreateWall (square.centerRight, square.centerTop);
+                TryCreateWall (square.centerBottom, square.centerLeft);
+                TryCreateWall (square.centerRight, square.centerTop);
                 break;
 
                 // 3 point:
             case 7:
                 MeshFromPoints (square.centerTop, square.topRight, square.bottomRight, square.bottomLeft, square.centerLeft);
-                CreateWall (square.centerLeft, square.centerTop);
+                TryCreateWall (square.centerLeft, square.centerTop);
                 break;
             case 11:
                 MeshFromPoints (square.topLeft, square.centerTop, square.centerRight, square.bottomRight, square.bottomLeft);
-                CreateWall (square.centerTop, square.centerRight);
+                TryCreateWall (square.centerTop, square.centerRight);
                 break;
             case 13:
                 MeshFromPoints (square.topLeft, square.topRight, square.centerRight, square.centerBottom, square.bottomLeft);
-                CreateWall (square.centerRight, square.centerBottom);
+                TryCreateWall (square.centerRight, square.centerBottom);
                 break;
             case 14:
                 MeshFromPoints (square.topLeft, square.topRight, square.bottomRight, square.centerBottom, square.centerLeft);
-                CreateWall (square.centerBottom, square.centerLeft);
+                TryCreateWall (square.centerBottom, square.centerLeft);
                 break;
 
                 // 4 point:
@@ -230,14 +278,14 @@ public class MeshGenerator : MonoBehaviour {
 
         spawnPoints.Add (new Vector2 (squareGrid.squares.GetLength (0) / 2, squareGrid.squares.GetLength (1) / 2));
         while (spawnPoints.Count > 0) {
-            int spawnIndex = Random.Range (0, spawnPoints.Count);
+            int spawnIndex = UnityEngine.Random.Range (0, spawnPoints.Count);
             Vector2 spawnCentre = spawnPoints[spawnIndex];
             bool candidateAccepted = false;
 
             for (int i = 0; i < attempts; i++) {
-                float angle = Random.value * Mathf.PI * 2;
+                float angle = UnityEngine.Random.value * Mathf.PI * 2;
                 Vector2 dir = new Vector2 (Mathf.Sin (angle), Mathf.Cos (angle));
-                Vector2 candidate = spawnCentre + dir * Random.Range (radius, 2 * radius);
+                Vector2 candidate = spawnCentre + dir * UnityEngine.Random.Range (radius, 2 * radius);
 
                 if (IsValid (candidate, cellSize, radius, points, grid)) {
                     points.Add (candidate);
@@ -333,6 +381,9 @@ public class MeshGenerator : MonoBehaviour {
 
     public class SquareGrid {
         public Square[, ] squares;
+        private float squareSize;
+
+        public SquareGrid () { }
 
         public SquareGrid (TileType[, ] map, float squareSize) {
             int nodeCountX = map.GetLength (0);
@@ -345,7 +396,7 @@ public class MeshGenerator : MonoBehaviour {
             for (int x = 0; x < nodeCountX; x++) {
                 for (int y = 0; y < nodeCountY; y++) {
                     Vector3 pos = new Vector3 (-mapWidth / 2 + x * squareSize + squareSize / 2, 0, -mapHeight / 2 + y * squareSize + squareSize / 2);
-                    controlNodes[x, y] = new ControlNode (pos, map[x, y] == TileType.Wall, squareSize);
+                    controlNodes[x, y] = new ControlNode (pos, map[x,y] == TileType.Wall, squareSize);
                 }
             }
 
@@ -397,6 +448,7 @@ public class MeshGenerator : MonoBehaviour {
     public class ControlNode : Node {
         public bool active;
         public Node above, right;
+        private float squareSize;
 
         public ControlNode (Vector3 _pos, bool _active, float squareSize) : base (_pos) {
             active = _active;
